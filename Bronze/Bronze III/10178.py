89503c2a5fa37e391af8fe705844f2fde8745f1b
Yeomoon <73222399+youmoon@users.noreply.github.com>
2025-09-24 23:07:42 +0900

    10178 - 맞았습니다!!

--- Bronze/Bronze III/10178.py
+++ Bronze/Bronze III/10178.py
@@ -1,5 +1,5 @@
 n = int(input())
 for t in range(n):
   i = input()
-  c, v = i.split()[0], i.split()[1]
+  c, v = int(i.split()[0]), int(i.split()[1])
   print(f"You get {c // v} piece(s) and your dad gets {c % v} piece(s).")

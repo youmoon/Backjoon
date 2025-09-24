ec4c46ca1fb076b983e35233eafb929cec35aac3
Yeomoon <73222399+youmoon@users.noreply.github.com>
2025-09-24 14:22:47 +0000

    28214 - 20점

---
+++ Bronze/Bronze III/28214.py
@@ -0,0 +1,11 @@
+i = input()
+n, k, p = int(i.split()[0]), int(i.split()[1]), int(i.split()[2])
+b = input().split()
+
+s = 0
+for t in range(n):
+    c = 0
+    for h in range(k):
+        if b[(t * k) + h] == '1': c += 1
+    if c >= p: s += 1
+print(s)

3556256686e50a25916d917f6fba8f0c71c95118
Yeomoon <73222399+youmoon@users.noreply.github.com>
2025-09-24 14:50:08 +0000

    34460 - 틀렸습니다

---
+++ Silver/Silver V/34460.py
@@ -0,0 +1,17 @@
+i = int(input())
+for t in range(i):
+    l, s = int(input()), input().split()
+    it = -1
+    for r in range(l):
+        if s[r] == "M":
+            if (it == -1) or (it == 2): it = 0
+            else: print("NO"); break
+        elif s[r] == "I":
+            if it == 0: it = 1
+            else: print("NO"); break
+        elif s[r] == "T":
+            if it == 1: it = 2
+            else: print("NO"); break
+        else: print("NO"); break
+    if it == 2: print("YES")
+    else: print("NO")

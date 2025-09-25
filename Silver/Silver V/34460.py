8b35620f0207637ae98f1d805fee65139b5008d7
Yeomoon <73222399+youmoon@users.noreply.github.com>
2025-09-25 07:17:33 +0000

    34460 - 맞았습니다!!

--- Silver/Silver V/34460.py
+++ Silver/Silver V/34460.py
@@ -1,18 +1,17 @@
 i = int(input())
 for t in range(i):
-    l, s = abs(int(input())), list(input())
+    l, s = int(input()), list(input())
     it = -1
     for r in range(l):
         if s[r] == "M":
             if (it == -1) or (it == 2): it = 0
-            else: it = -1; break
+            else: it = -2
         elif s[r] == "I":
-            if it == 0: it = 1
-            else: it = -1; break
+            if (it == 0) or (it == 2): it = 1
+            else: it = -2
         elif s[r] == "T":
             if it == 1: it = 2
-            else: it = -1; break
-        else: it = -1; break
-    if l != len(s): print("NO")
-    elif it == 2: print("YES")
+            else: it = -2
+        else: it = -2
+    if it == 2: print("YES")
     else: print("NO")

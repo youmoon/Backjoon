fba6dcaf7cd5614ffbf54b42c857f708681f63ce
Yeomoon <73222399+youmoon@users.noreply.github.com>
2025-09-26 15:15:56 +0900

    13410 - 맞았습니다!!

--- Bronze/Bronze II/13410.py
+++ Bronze/Bronze II/13410.py
@@ -1,5 +1,4 @@
 i = input()
-n, k, h = int(i.split()[0]), int(i.split()[0]), 0
-for t in range(k):
-    if int(str(n * (t + 1))[::-1]) > h: h = int(str(n * (t + 1))[::-1])
-print(h)
+n, k, a = int(i.split()[0]), int(i.split()[1]), []
+for t in range(k): a.append(int(str(n * (t + 1))[::-1]))
+print(max(a))
